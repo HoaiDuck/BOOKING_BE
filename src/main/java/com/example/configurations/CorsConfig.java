6a8398cc48571fd5deb087bac6b2d07f3f4b6905
huydq <huyquang9052@gmail.com>
2025-07-15 17:13:30 +0700

    CorsConfig đã xong

---
+++ src/main/java/com/example/configurations/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.example.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            public void addCorsMapping(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedOrigins("https:localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
